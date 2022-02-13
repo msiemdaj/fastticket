7d97b68482ad92e1387dcba03d704fdc0974767f
msiemdaj <epsadpersonal@gmail.com>
2022-02-13 16:20:11 +0100

    Login

--- resources/js/Pages/Auth/Login.js
+++ resources/js/Pages/Auth/Login.js
@@ -1,9 +1,75 @@
-import React from "react"
+import React, { useState } from "react"
+import { Inertia } from '@inertiajs/inertia'
 
-const HomePage = () => {
+const Login = () => {
+
+    const [loginInput, setLogin] = useState({
+        email: '',
+        password: '',
+        remember: false,
+    });
+
+    const handleInput = (e) => {
+        setLogin({
+            ...loginInput, [e.target.name]: e.target.value
+        });
+    }
+
+    const loginSubmit = async (e) => {
+        e.preventDefault();
+        Inertia.post('/login', loginInput);
+    }
     return (
-        <p>Login page</p>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">'Login</div>
+
+                        <div className="card-body">
+                            <form onSubmit={loginSubmit}>
+                                <div className="form-group row">
+                                    <label htmlFor="email" className="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+
+                                    <div className="col-md-6">
+                                        <input onChange={handleInput} value={loginInput.email}
+                                            id="email" type="email" className="form-control" name="email" required />
+                                    </div>
+                                </div>
+                                <div className="form-group row">
+                                    <label htmlFor="password" className="col-md-4 col-form-label text-md-right">Password</label>
+
+                                    <div className="col-md-6">
+                                        <input onChange={handleInput} value={loginInput.password}
+                                            id="password" type="password" className="form-control" name="password" required />
+                                    </div>
+                                </div>
+
+
+                                <div className="form-group row">
+                                    <div className="col-md-6 offset-md-4">
+                                        <div className="form-check">
+                                            <input className="form-check-input" type="checkbox" name="remember" id="remember" />
+
+                                            <label className="form-check-label" htmlFor="remember">
+                                                Remember Me
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="form-group row mb-0">
+                                    <div className="col-md-8 offset-md-4">
+                                        <button type="submit" className="btn btn-primary">Login</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default HomePage
+export default Login
