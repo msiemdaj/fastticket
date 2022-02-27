bbb1a7178fc30d64b7f8785b7170b96997502689
msiemdaj <epsadpersonal@gmail.com>
2022-02-27 16:43:06 +0100

    Default role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->enum('role', UserRole::TYPES)->default('user');
+            $table->enum('role', UserRole::TYPES)->default(UserRole::USER);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
